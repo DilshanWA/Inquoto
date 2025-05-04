64d2449056caa7cafcbb99d51adaea99a96115c5
AThusithaGayan <thusithag@aplicy.com>
2025-05-04 21:47:17 +0530

    Create: create authentications functions ,register and login

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,19 @@
-const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
-const invoiceRoutes = require("./routes/invoiceRoutes");
+// const invoiceRoutes = require("./routes/invoiceRoutes");
+const { Register ,Login} = require("./middleware/authMiddleware");
 
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.use("/api/invoices", invoiceRoutes);
+// app.use("/api/invoices", invoiceRoutes);
+app.post("/register", Register); 
+app.post("/login", Login); 
 
-exports.api = functions.https.onRequest(app);
+
+
+app.listen(5000, () => {
+  console.log("Server running at http://localhost:5000");
+});
--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,21 +1,12 @@
 
+var admin = require("firebase-admin");
 
-// Backend/firebase.js
+var serviceAccount = require("../../../Service Account/serviceAccountKey.json");
 
-const admin = require("firebase-admin");
-
-if (!admin.apps.length) {
-  const serviceAccount = require("../config/serviceAccountKey.json");
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "",
-  });
-}
-
-const db = admin.firestore();
-const bucket = admin.storage().bucket();
-
-module.exports = { db, bucket };
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://inventrydb-d9b53-default-rtdb.firebaseio.com"
+});
 
 
+module.exports = { admin };
